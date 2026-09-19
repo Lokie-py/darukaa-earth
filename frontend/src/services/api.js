b874e9aa38e0f147b72d38c4e1dd7ad2b5f33895
Sonar Lokesh Nandu <lokeshsonar1425@gmail.com>
2026-09-19 16:01:12 +0530

    Fix production API connection

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://darukaa-earth-kl4z.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
